815fd0a39b24c1a57282f4115882b6ec4f92927b
yangjz <yangjz1125@outlook.com>
2019-04-17 22:08:33 +0800

    Remove verbose settings

--- init.el
+++ init.el
@@ -3,17 +3,3 @@
 (require 'cl)
 
 (require 'init-packages)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (popwin ace-window ace-jump-mode yasnippet-snippets yasnippet flycheck company smex powerline monokai-theme undo-tree smartparens highlight-parentheses))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
